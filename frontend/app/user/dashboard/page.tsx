3d576d941fa8f92220b6d326b3b977e365aa6eda
Aryan <aaryan.nakarmi111@gmail.com>
2026-07-19 11:27:42 +0545

    dashboard herosection fixed

--- frontend/app/user/dashboard/page.tsx
+++ frontend/app/user/dashboard/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { HiArrowRight } from "react-icons/hi";
+import { HiArrowRight } from "react-icons/hi"; 
 import { toast } from "react-toastify";
 import { getMyReports, LostItemReport } from "@/lib/api/lost-item/lost-item";
 export default function DashboardPage() {
