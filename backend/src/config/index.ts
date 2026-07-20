6df3619494da55fa3a9b76a51068a20990e81aef
Aryan <aaryan.nakarmi111@gmail.com>
2026-07-20 21:46:34 +0545

    fix mongodb ipv6 connection error by forcing ipv4

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -5,7 +5,7 @@ export const PORT: number =
     process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 export const MONGODB_URI: string = 
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultdb';
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/defaultdb';
     
 
 export const JWT_SECRET: string = 
